27240b988cc98b92ca5eef3159366369756102bf
kimu805 <kimukimuok805@gmail.com>
2024-07-03 17:14:34 +0900

    set up routing "comments#create"

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "tweets#index"
-  resources :tweets
+  resources :tweets do
+    reosurces :comments, only: :create
+  end
   resources :users, only: :show
 end
